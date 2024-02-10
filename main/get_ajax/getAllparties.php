330f800c28c513b3ed57ac22efa565dce93a0e94
cometdev321 <nerddev321@gmail.com>
2024-02-10 13:29:02 +0530

    simple edits

--- main/get_ajax/getAllparties.php
+++ main/get_ajax/getAllparties.php
@@ -12,9 +12,9 @@ if (mysqli_num_rows($result) > 0) {
         <?php while ($row = mysqli_fetch_array($result)) { 
                 $party=$row['id'];
 
-                $querySalesTotal="select sum(total_balance) as totalSales from tblsalesinvoices where userID='$session' and party_name='$party'";
-                $queryPurchaseTotal="select sum(total_balance) as totalPurchase from tblpurchaseinvoices where userID='$session' and party_name='$party'";
-                $queryPaymentIn="select sum(paymentAmount)as totalPayIn from tblpaymentin where userID='$session' and partyname='$party'";
+                $querySalesTotal="select sum(total_balance) as totalSales from tblsalesinvoices where userID='$session' and party_name='$party' and full_paid='NO'";
+                $queryPurchaseTotal="select sum(total_balance) as totalPurchase from tblpurchaseinvoices where userID='$session' and party_name='$party' and full_paid='NO'";
+                $queryPaymentIn="select sum(paymentAmount)as totalPayIn from tblpaymentin where userID='$session' and partyname='$party' ";
                 $queryPaymentOut="select sum(paymentAmount)as totalPayOut from tblpaymentout where userID='$session' and partyname='$party'";
 
                 $exeSalesTotal=mysqli_query($conn,$querySalesTotal);
@@ -27,20 +27,27 @@ if (mysqli_num_rows($result) > 0) {
                 $fetchPaymentIn=mysqli_fetch_array($exeSalesIn);
                 $fetchPaymentOut=mysqli_fetch_array($exePaymentOut);
 
-                $receive=$fetchSalesTotal['totalSales']-$fetchPaymentIn['totalPayIn'];
-                $payment=$fetchPurchaseTotal['totalPurchase']-$fetchPaymentOut['totalPayOut'];
+                $recievable=(($fetchSalesTotal['totalSales'])-($fetchPaymentIn['totalPayIn']));
+                $payable=(($fetchPurchaseTotal['totalPurchase'])-($fetchPaymentOut['totalPayOut']));
                 $credit=0;
-                if($payment>$receive){
-                    $credit=$receive;
+                if($recievable>=$payable){
+                    $recievable=$recievable-$payable;
+                    $payable=0;
                 }
+
+                if($payable>=$recievable){
+                    $payable=$payable-$recievable;
+                    $recievable=0;
+                }
+
 
             ?>
             <tr>
                 <td><?php echo $slno; ?></td>
                 <td><?php echo $row['name']; ?></td>
                 <td><?php echo $row['mobno']; ?></td>
-                <td>&#8377;<?php echo $payment>$receive?'0':$receive; ?></td>
-                <td>&#8377;<?php echo $payment; ?></td>
+                <td>&#8377;<?php echo $recievable; ?></td>
+                <td>&#8377;<?php echo $payable; ?></td>
                 <td>&#8377;<?php echo abs($credit); ?></td>
             </tr> 
             <?php $slno++;

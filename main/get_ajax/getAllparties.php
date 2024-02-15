054bac7a2537b62f6de22bd8454176cc39f1e12a
athul <athulkharvi1234@gmail.com>
2024-02-15 14:03:55 +0530

    all party report added

--- main/get_ajax/getAllparties.php
+++ main/get_ajax/getAllparties.php
@@ -3,9 +3,33 @@ include('../common/cnn.php');
 include('../common/session_control.php');
 
 
+
+$query = "UPDATE tblpartyreport 
+          SET 
+            r_balance = CASE 
+                          WHEN r_balance > p_balance THEN r_balance - p_balance 
+                          ELSE 0 
+                        END,
+            p_balance = CASE 
+                          WHEN p_balance > r_balance THEN p_balance - r_balance 
+                          ELSE 0 
+                        END";
+
+$result = mysqli_query($conn, $query);
+
+if ($result) {
+    echo "Update successful";
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
+
+
 $slno = 1;
 $credit;
 $query = "SELECT `id`,`name`,`mobno` from tblparty where userID='$session'";
+
+$query = "SELECT pr.*,p.name AS pname FROM tblpartyreport pr JOIN tblparty p ON pr.partyname = p.id WHERE pr.userID = '$session'";
+
 $result = mysqli_query($conn, $query);
 if (mysqli_num_rows($result) > 0) {
     ?>
@@ -33,14 +57,14 @@ if (mysqli_num_rows($result) > 0) {
                     //receivables are 
                     $fetchSalesTotal['totalSales'] 
 
+
             ?>
             <tr>
                 <td><?php echo $slno; ?></td>
-                <td><?php echo $row['name']; ?></td>
+                <td><?php echo $row['pname']; ?></td>
                 <td><?php echo $row['mobno']; ?></td>
-                <td>&#8377;<?php echo $recievable; ?></td>
-                <td>&#8377;<?php echo $payable; ?></td>
-                <td>&#8377;<?php echo abs($credit); ?></td>
+                <td><?php echo $row['r_balance']; ?></td>
+                <td><?php echo $row['p_balance']; ?></td>
             </tr> 
             <?php $slno++;
         } ?>
@@ -52,4 +76,14 @@ if (mysqli_num_rows($result) > 0) {
         </tr>
 <?php
 }
+
+
+
+
+
+
+
+
+
+
 ?>
